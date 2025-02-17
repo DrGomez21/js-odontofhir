bccf1beca9d38bfd50f1edff0dd68f3f5c912662
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-02-17 10:30:00 -0300

    Agregar funciones para crear y eliminar pacientes en la API FHIR

--- src/api/fhir.api.js
+++ src/api/fhir.api.js
@@ -3,7 +3,15 @@ import axios from "axios";
 const PATIENT_BASE_URL = 'http://localhost:8080/fhir/Patient/'
 
 export const getAllPatients = async () => {
-  console.log('Llamada a API')
   const response = await axios.get(PATIENT_BASE_URL)
   return response.data
+}
+
+export const postPatient = async (newPatient) => {
+  const response = await axios.post(PATIENT_BASE_URL, newPatient)
+  return response
+}
+
+export const deletePatientById = async (PatientId) => {
+  
 }
