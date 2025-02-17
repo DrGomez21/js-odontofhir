fb8d4526a02e20f8a5cf5bb14ebf9c109a7e900c
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-02-17 10:30:35 -0300

    Agregar botón para añadir pacientes en la página de pacientes

--- src/pages/PatientPage.jsx
+++ src/pages/PatientPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { PatientTable } from '../components/PatientTable'
+import { Link } from 'react-router-dom'
 
 export const PatientPage = () => {
   return (
-    <div>
+    <div className='flex flex-col'>
       PatientPage
+      <div className='flex justify-end p-4'>
+        <Link to={'/patient-form'}>
+          <button className='end-0 px-4 py-2 rounded-lg border-sky-600 bg-sky-200 text-blue-500 font-semibold hover:cursor-pointer'>
+            Añadir paciente
+          </button>
+        </Link>
+      </div>
       <PatientTable />
     </div>
   )
