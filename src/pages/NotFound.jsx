65487284ca0c8b09afbbe488ef6dbda2308136ed
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-02-18 16:00:50 -0300

    Mejorar la página de error 404 con imagen y estilos actualizados

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import NotFoundImage from '../assets/pagenotfound.svg'
 
 export const NotFound = () => {
   return (
-    <div style={{textAlign: 'center', marginTop: '50px'}}>
-      <h1>Esta página no se encontró</h1>
-      <p>Lo sentimos, la página que estás buscando no existe.</p>
+    <div className='flex flex-col gap-2 items-center justify-center mt-8'>
+      <img src={NotFoundImage} className='w-80' />
+      <h1 className='font-semibold'>Esta página no se encontró</h1>
+      <p className='font-light'>Lo sentimos, la página que estás buscando no existe.</p>
     </div>
   )
 }
