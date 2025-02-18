b75b00ad0eee8b08edb865f5bfbda8e8cb02769d
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-02-18 16:01:34 -0300

    Agregar componentes Loader y Error a PatientProfile, mejorar estilos y estructura de la página

--- src/pages/PatientProfile.jsx
+++ src/pages/PatientProfile.jsx
@@ -4,6 +4,8 @@ import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import { PatientDetail } from '../components/PatientDetail'
 import { FichaClinica } from '../components/FichaClinica'
+import Loader from '../components/basics/Loader'
+import { Error } from '../components/basics/Error'
 
 const getPatientById = async (id) => {
   console.log(`Buscando información del paciente ${id}`)
@@ -20,14 +22,24 @@ export const PatientProfile = () => {
     staleTime: 1000 * 60 * 5,
   })
 
-  if (isLoading) return <p>Cargando...</p>
-  if (isError) return <p>Error {error.message}</p>
+  if (isLoading) return <Loader />
+  if (isError) return <Error message={error.message} />
 
   return (
-    <div className='flex flex-col'>
-      <h1>PatientProfile</h1>
-      <PatientDetail patient={data} />
-      <h2>Ficha clínica</h2>
+    <div className='flex flex-col mx-8 py-6 gap-4'>
+      <h1 className='font-bold text-[#4A4A4A] text-4xl'>Perfil del paciente</h1>
+      <div className='flex w-full justify-between gap-12 items-center'>
+        <PatientDetail patient={data} />
+        <div>
+          <button
+            className='flex items-center gap-2 px-4 py-2 rounded-md border-2 border-gray-600 bg-[#fcbeb3] text-[#4A4A4A] font-semibold hover:cursor-pointer hover:bg-[#f09686] hover:scale-95 duration-100 transition-all'
+          >
+            <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
+            Eliminar
+          </button>
+        </div>
+      </div>
+      <h2 className='font-bold text-[#4A4A4A] text-3xl'>Ficha clínica</h2>
       <FichaClinica />
     </div>
   )
