67d5dbb52d58942216938f4133ada506082c4056
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-02-18 16:01:00 -0300

    Agregar componente PatientFormCreate con encabezado y formulario de registro de pacientes

---
+++ src/pages/PatientFormCreate.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { PatientForm } from '../components/PatientForm'
+import { Header } from '../components/basics/Header'
+
+export const PatientFormCreate = () => {
+  return (
+    <div className='flex flex-col'>
+      <Header />
+      <h1 className='font-bold text-center px-8 mt-4 mb-4 text-3xl text-[#4a4a4a]'> Registro de pacientes </h1>
+      <div className='flex flex-col items-center justify-center'>
+        <PatientForm />
+      </div>
+
+    </div>
+  )
+}
