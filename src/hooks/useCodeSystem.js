90c4778772044d723fa36626bff519491a351bcb
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-04-28 15:18:16 -0300

    Agregar hook useCodeSystem odontológicos desde la API

---
+++ src/hooks/useCodeSystem.js
@@ -0,0 +1,81 @@
+import { useQuery } from "@tanstack/react-query"
+import {
+  getBarrios,
+  getCiudades,
+  getDepartamentos,
+  getHallazgosOdontologicos,
+  getIdentificaciones,
+  getNacionalidades,
+  getProcedimientosOdontologicos,
+  getPueblosIndigenas,
+  getSeccionesFicha
+} from "../api/codesystem/codesystem.action.js"
+
+export const useCodeSystem = () => {
+
+  const barriosDelParaguay = useQuery({
+    queryKey: ['barrio'],
+    queryFn: getBarrios,
+    staleTime: 1000 * 60 * 60 * 72,
+  })
+
+  const ciudadesDelParaguay = useQuery({
+    queryKey: ['ciudades'],
+    queryFn: getCiudades,
+    staleTime: 1000 * 60 * 60 * 72,
+  })
+
+  const departamentosDelParaguay = useQuery({
+    queryKey: ['departamentos'],
+    queryFn: getDepartamentos,
+    staleTime: 1000 * 60 * 60 * 72,
+  })
+
+  const hallazgos = useQuery({
+    queryKey: ['hallazgos'],
+    queryFn: getHallazgosOdontologicos,
+    staleTime: 1000 * 60 * 60 * 72,
+  })
+
+  const identificaciones = useQuery({
+    queryKey: ['identificaciones'],
+    queryFn: getIdentificaciones,
+    staleTime: 1000 * 60 * 60 * 72,
+  })
+
+  const nacionalidades = useQuery({
+    queryKey: ['nacionalidades'],
+    queryFn: getNacionalidades,
+    staleTime: 1000 * 60 * 60 * 72,
+  })
+
+  const pueblosIndigenas = useQuery({
+    queryKey: ['pueblosIndigenas'],
+    queryFn: getPueblosIndigenas,
+    staleTime: 1000 * 60 * 60 * 72,
+  })
+
+  const seccionesFichaClinica = useQuery({
+    queryKey: ['seccionesFichaClinica'],
+    queryFn: getSeccionesFicha,
+    staleTime: 1000 * 60 * 60 * 72,
+  })
+
+  const procedimientosOdontologicos = useQuery({
+    queryKey: ['seccionesFichaClinica'],
+    queryFn: getProcedimientosOdontologicos,
+    staleTime: 1000 * 60 * 60 * 72,
+  })
+
+  return {
+    barriosDelParaguay,
+    ciudadesDelParaguay,
+    departamentosDelParaguay,
+    hallazgos,
+    identificaciones,
+    nacionalidades,
+    pueblosIndigenas,
+    seccionesFichaClinica,
+    procedimientosOdontologicos,
+  }
+}
