f5dd9114df992c645090b586042e108ffcf45cb1
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-04-29 19:26:38 -0300

    Agregar espacio entre elementos en el formulario de registro de pacientes

--- src/pages/PatientFormCreate.jsx
+++ src/pages/PatientFormCreate.jsx
@@ -4,7 +4,7 @@ import { Header } from '../components/basics/Header'
 
 export const PatientFormCreate = () => {
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col gap-4'>
       <Header />
       <h1 className='font-bold text-center px-8 mt-4 mb-4 text-3xl text-[#4a4a4a]'> Registro de pacientes </h1>
       <div className='flex flex-col items-center justify-center'>
