1e7ee2f69aa3390d83496264b3271a1917a3c129
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-04-29 19:25:59 -0300

    Agregar enlace al logo en el encabezado para mejorar la navegación

--- src/components/basics/Header.jsx
+++ src/components/basics/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Logo from '../../assets/logo.svg'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <div className='flex px-6 mt-4'>
-      <img src={Logo} />
+      <Link to='/'>
+        <img src={Logo} />
+      </Link>
     </div>
   )
 }
