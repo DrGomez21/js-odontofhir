2e4af5dca79ffc22de4f4e7f285195d2730ef860
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-05-04 15:46:42 -0300

    Refactor componente Indicator para utilizar estados en inglés y ajustar clases de estilo

--- src/components/Indicator.jsx
+++ src/components/Indicator.jsx
@@ -4,9 +4,10 @@ export const Indicator = ({ estado }) => {
   return (
     <div
       className={`
-        ${estado === 'Finalizado' ? 'bg-green-100 text-sm text-green-400 border border-green-400' : ''}
-        ${estado === 'En proceso' ? 'bg-sky-100 text-sm text-sky-400 border border-sky-400' : ''}
-      font-medium px-2 rounded-md`}
+        ${estado === 'finished' ? 'bg-green-100 text-sm text-green-400 border border-green-400' : ''}
+        ${estado === 'in-progress' ? 'bg-sky-100 text-sm text-sky-400 border border-sky-400' : ''}
+        ${estado === 'cancelled' ? 'bg-red-100 text-sm text-red-400 border border-red-400' : ''}
+      font-medium px-2 rounded-md w-fit`}
     >
       {estado}
     </div>
