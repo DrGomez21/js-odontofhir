1ed614b0d21160119cd74ebbe3c0bef9e50c2ce1
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-05-05 12:34:41 -0300

    Eliminar manejo de errores y simplificar la función addHallazgo para retornar directamente los datos de la respuesta.

--- src/api/hallazgos/add-hallazgo.action.js
+++ src/api/hallazgos/add-hallazgo.action.js
@@ -1,15 +1,6 @@
 import { odontoFhirApi } from "../fhir.api";
 
 export const addHallazgo = async (hallazgo) => {
-  try {
-    const response = await odontoFhirApi.post('/Condition', hallazgo, {
-      headers: {
-        'Content-Type': 'application/fhir+json',
-      },
-    });
-    return response
-  } catch (error) {
-    console.error('Error al agregar el hallazgo:', error);
-    throw error;
-  }
+  const { data } = await odontoFhirApi.post('/Condition', hallazgo);
+  return data
 }
