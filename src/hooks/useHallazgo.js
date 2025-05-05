04b4fa449312c1c122ea2e400b2fb51bbe86309c
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-05-05 12:30:47 -0300

    Refactor useHallazgo: agregar lógica de mutación y corregir claves de consulta para hallazgos

--- src/hooks/useHallazgo.js
+++ src/hooks/useHallazgo.js
@@ -1,38 +1,44 @@
-import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
+import { useQuery, useQueryClient, useMutation, QueryClient } from "@tanstack/react-query";
 import { addHallazgo } from "../api/hallazgos/add-hallazgo.action";
 import { getHallazgoByEncounterId, getHallazgoByPatientId, getHallazgoByPatientIdAndToothCode } from "../api/hallazgos/get-hallazgo.action";
 
 export const useHallazgo = (encounterId, paciente, diente) => {
-  
-  // const queryClient = useQueryClient();
+
+  const queryClient = useQueryClient();
 
   // Querys
   const hallazgosByEncounter = useQuery({
     queryKey: ["hallazgos", encounterId],
     queryFn: () => getHallazgoByEncounterId(encounterId),
-    staleTime: 1000 * 60 * 5,
     enabled: !!encounterId,
   });
 
   const hallazgosByPatient = useQuery({
     queryKey: ["hallazgos", paciente],
     queryFn: () => getHallazgoByPatientId(paciente),
-    staleTime: 1000 * 60 * 5,
     enabled: !!paciente,
   });
 
   const hallazgoByPatientAndTooth = useQuery({
-    queryKey: ['hallazgo', paciente, diente],
+    queryKey: ["hallazgos", paciente, diente],
     queryFn: () => getHallazgoByPatientIdAndToothCode(paciente, diente),
-    staleTime: 1000 * 60 * 5,
     enabled: !!diente
   })
 
   // Mutations
+  const hallazgoMutation = useMutation({
+    mutationFn: addHallazgo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["hallazgos", paciente] })
+      queryClient.refetchQueries(["hallazgos", paciente])
+      queryClient.refetchQueries(["hallazgos", paciente, diente])
+    }
+  })
 
   return {
     hallazgosByEncounter,
     hallazgosByPatient,
     hallazgoByPatientAndTooth,
+    hallazgoMutation,
   }
 }
