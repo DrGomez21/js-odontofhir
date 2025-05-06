9620353f6ad19a9876da96b4b8554764a67369f4
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-05-06 13:17:25 -0300

    Refactor FichaClinica para utilizar un diseño de cuadrícula y mejorar la disposición de componentes.

--- src/components/FichaClinica.jsx
+++ src/components/FichaClinica.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Odontograma } from './Odontograma'
+import { Encounter } from './Sidebar'
 
 export const FichaClinica = ({ patient }) => {
   return (
-    <div className='flex gap-4'>
-      <div className='w-full'>
+    <div className='grid grid-cols-2 gap-4'>
+      <div className='col-span-1'>
         <Odontograma patient={patient} />
+      </div>
+      <div className='col-span-1 bg-white rounded-md p-2'>
+        <Encounter patientId={patient.id} />
       </div>
     </div>
   )
