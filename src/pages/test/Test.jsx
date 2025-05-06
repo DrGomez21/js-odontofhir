6fcdf8ecb90f97ef8740d0a9f4079088976a5b22
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-05-06 11:07:08 -0300

    Eliminar lógica y estado innecesarios en el componente Test para simplificar el código.

--- src/pages/test/Test.jsx
+++ src/pages/test/Test.jsx
@@ -1,35 +1,9 @@
-import React, { useState } from 'react'
-import { useCodeSystem } from '../../hooks/useCodeSystem'
 
 export const Test = () => {
-
-  const { departamentosDelParaguay, nacionalidades } = useCodeSystem()
-  const [showCiudades, setShowCiudades] = useState(false)
-
-  if (nacionalidades.isLoading) {
-    return <div>Cargando departamentos...</div>
-  }
-
-  const handleCiudadesClick = () => {
-    setShowCiudades(!showCiudades)
-    console.log(nacionalidades.data)
-  }
 
   return (
     <div>
       <h1>Test API</h1>
-      <button onClick={handleCiudadesClick}>
-        Mostrar departamentos
-      </button>
-      {showCiudades && (
-        <ul>
-          {nacionalidades.data.map((departamento) => (
-            <li key={departamento.code}>
-              {departamento.display}
-            </li>
-          ))}
-        </ul>
-      )}
     </div>
   )
 }
