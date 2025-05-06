bd7f8adede6044074b0eb052beed87b596e52114
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-05-06 11:06:50 -0300

    Refactor hooks en useCodeSystem para mejorar la claridad y la estructura del código, y optimizar la mutación en useHallazgo para un manejo más eficiente de la caché.

--- src/hooks/useCodeSystem.js
+++ src/hooks/useCodeSystem.js
@@ -12,78 +12,82 @@ import {
   getSeccionesFicha
 } from "../api/codesystem/codesystem.action.js"
 
-export const useCodeSystem = () => {
-
-  const anatomiaDental = useQuery({
+export const useAnatomiaDental = () => {
+  return useQuery({
     queryKey: ['anatomiaDental'],
     queryFn: getAnatomiaDental,
     staleTime: 1000 * 60 * 60 * 72,
   })
+} 
 
-  const barriosDelParaguay = useQuery({
-    queryKey: ['barrio'],
+export const useBarriosDelParaguay = () => {
+  return useQuery({
+    queryKey: ['barrios'],
     queryFn: getBarrios,
     staleTime: 1000 * 60 * 60 * 72,
   })
+} 
 
-  const ciudadesDelParaguay = useQuery({
+export const useCiudadesDelParaguay = () => {
+  return useQuery({
     queryKey: ['ciudades'],
     queryFn: getCiudades,
     staleTime: 1000 * 60 * 60 * 72,
   })
+} 
 
-  const departamentosDelParaguay = useQuery({
+export const useDepartamentosDelParaguay = () => {
+  return useQuery({
     queryKey: ['departamentos'],
     queryFn: getDepartamentos,
     staleTime: 1000 * 60 * 60 * 72,
   })
+} 
 
-  const hallazgos = useQuery({
-    queryKey: ['hallazgos'],
+export const useHallazgos = () => {
+  return useQuery({
+    queryKey: ['codigoHallazgo'],
     queryFn: getHallazgosOdontologicos,
     staleTime: 1000 * 60 * 60 * 72,
   })
+} 
 
-  const identificaciones = useQuery({
+export const useIdentificaciones = () => {
+  return useQuery({
     queryKey: ['identificaciones'],
     queryFn: getIdentificaciones,
     staleTime: 1000 * 60 * 60 * 72,
   })
+} 
 
-  const nacionalidades = useQuery({
+export const useNacionalidades = () => {
+  return useQuery({
     queryKey: ['nacionalidades'],
     queryFn: getNacionalidades,
     staleTime: 1000 * 60 * 60 * 72,
   })
+} 
 
-  const pueblosIndigenas = useQuery({
+export const usePueblosIndigenas = () => {
+  return useQuery({
     queryKey: ['pueblosIndigenas'],
     queryFn: getPueblosIndigenas,
     staleTime: 1000 * 60 * 60 * 72,
   })
+} 
 
-  const seccionesFichaClinica = useQuery({
-    queryKey: ['seccionesFichaClinica'],
+export const useSeccionesFichaClinica = () => {
+  return useQuery({
+    queryKey: ['codigoSeccionesFichaClinica'],
     queryFn: getSeccionesFicha,
     staleTime: 1000 * 60 * 60 * 72,
   })
+} 
 
-  const procedimientosOdontologicos = useQuery({
-    queryKey: ['seccionesFichaClinica'],
+export const useProcedimientosOdontologicos = () => {
+  return useQuery({
+    queryKey: ['codigoProcedimientos'],
     queryFn: getProcedimientosOdontologicos,
     staleTime: 1000 * 60 * 60 * 72,
   })
-
-  return {
-    barriosDelParaguay,
-    ciudadesDelParaguay,
-    departamentosDelParaguay,
-    hallazgos,
-    identificaciones,
-    nacionalidades,
-    pueblosIndigenas,
-    seccionesFichaClinica,
-    procedimientosOdontologicos,
-    anatomiaDental,
-  }
-}
+} 
--- src/hooks/useHallazgo.js
+++ src/hooks/useHallazgo.js
@@ -1,6 +1,7 @@
 import { useQuery, useQueryClient, useMutation, QueryClient } from "@tanstack/react-query";
 import { addHallazgo } from "../api/hallazgos/add-hallazgo.action";
 import { getHallazgoByEncounterId, getHallazgoByPatientId, getHallazgoByPatientIdAndToothCode } from "../api/hallazgos/get-hallazgo.action";
+import { odontoFhirApi } from "../api/fhir.api";
 
 export const useHallazgo = (encounterId, paciente, diente) => {
 
@@ -26,12 +27,48 @@ export const useHallazgo = (encounterId, paciente, diente) => {
   })
 
   // Mutations
+  // const hallazgoMutation = useMutation({
+  //   mutationFn: addHallazgo,
+  //   onSuccess: () => {
+  //     console.log('Se agrego el hallazgo.')
+  //     queryClient.invalidateQueries({ queryKey: ["hallazgos", paciente] })
+  //     queryClient.invalidateQueries({ queryKey: ["hallazgos", paciente, diente] })
+  //     console.log('Se invalidaron las queries.')
+  //   },
+  // })
+
   const hallazgoMutation = useMutation({
     mutationFn: addHallazgo,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["hallazgos", paciente] })
-      queryClient.refetchQueries(["hallazgos", paciente])
-      queryClient.refetchQueries(["hallazgos", paciente, diente])
+    onSuccess: async newHallazgo => {
+      // Insert optimista en caché.
+      queryClient.setQueryData(
+        ['hallazgos', paciente],
+        prev => ({
+          ...prev,
+          entry: [
+            ...prev.entry,
+            { resource: newHallazgo }
+          ]
+        })
+      )
+
+      // refetch oficial
+      await queryClient.invalidateQueries(['hallazgos', paciente])
+
+      // recuperar el recurso nuevo por su _id
+      const { data: bundleById } = await odontoFhirApi.get(`/Condition?subject=Patient/${paciente}&_id=${newHallazgo.id}`)
+      // bundleById.entry[0].resource será tu recurso exacto
+      // Volver a mergearlo:
+      queryClient.setQueryData(
+        ['hallazgos', paciente],
+        prev => ({
+          ...prev,
+          entry: [
+            ...prev.entry.filter(e => e.resource.id != newHallazgo.id),
+            bundleById.entry[0]
+          ]
+        })
+      )
     }
   })
 
