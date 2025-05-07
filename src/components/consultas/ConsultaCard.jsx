aa690b4d73bd9a16c8cca9497f3bdc6ebcfa02fe
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-05-07 16:26:53 -0300

    Agregar el parámetro paciente al componente ConsultaCard y mejorar la navegación a detalles de la consulta.

--- src/components/consultas/ConsultaCard.jsx
+++ src/components/consultas/ConsultaCard.jsx
@@ -1,21 +1,27 @@
 import { useState } from "react"
 import { Indicator } from "../Indicator";
-import { Edit, Eye } from "lucide-react";
+import { Eye } from "lucide-react";
+import { useNavigate, useParams } from "react-router-dom";
 
-export const ConsultaCard = ({ encounter }) => {
+export const ConsultaCard = ({ encounter, paciente }) => {
 
   const [visible, setVisible] = useState(false)
+  const navigate = useNavigate()
+
   const options = { timeZone: 'UTC', year: 'numeric', month: 'numeric', day: 'numeric' };
 
   const handleClick = () => {
-    setVisible(!visible)
+    console.log('HOLA')
+  }
+
+  const goToDetails = (id) => {
+    navigate(`/patient/${paciente}/consulta/${id}`)
   }
 
   return (
     <div
       className='bg-white p-2 border rounded-md m-1 hover:bg-gray-100 cursor-pointer transition-all duration-100'
-      onMouseEnter={handleClick}
-      onMouseLeave={handleClick}
+      onClick={() => goToDetails(encounter.id)}
     >
       <span className="font-medium text-[#4a4a4a]">Consulta del: {new Date(encounter.period.start).toLocaleDateString(undefined, options)}</span>
       <Indicator estado={encounter.status} />
