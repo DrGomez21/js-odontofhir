9bab66419d68ed8bb53489b751e0eb0057f73b2b
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-05-07 16:26:46 -0300

    Eliminar el registro de consola en el componente HallazgoCard para limpiar el código.

--- src/components/cards/HallazgoCard.jsx
+++ src/components/cards/HallazgoCard.jsx
@@ -14,7 +14,6 @@ export const HallazgoCard = ({condition}) => {
       className='bg-white p-2 border rounded-md m-1 hover:bg-gray-100 cursor-pointer transition-all duration-100'
       onClick={handleClick}
     >
-      {console.log(condition.bodySite[0].coding[0].code)}
       <p className="font-medium text-[#4a4a4a]">Diente: {obtenerNumeroPorCodigo(condition.bodySite[0].coding[0].code)}</p>
       <span className="font-medium text-[#4a4a4a]">Hallazgo: {condition.code.coding[0].display}</span>
     </div>
