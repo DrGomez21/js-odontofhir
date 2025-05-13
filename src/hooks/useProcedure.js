f1eb6c57cacf3575388616f51fb11d7dc7cad532
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-05-13 15:00:43 -0300

    Agregar hook useProcedure para gestionar procedimientos: obtener, añadir y manejar caché.

---
+++ src/hooks/useProcedure.js
@@ -0,0 +1,51 @@
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query"
+import { getProcedureById, getAllProcedures, getProcedureByEncounterId, getProcedureByPatientId, getProcedureByReference } from "../api/procedure/get-procedure.action"
+import { addProcedure } from "../api/procedure/add-procedure.action"
+
+export const useGetProcedureById = (id) => {
+  return useQuery({
+    queryKey: ['procedimiento', id],
+    queryFn: () => getProcedureById(id),
+  })
+}
+
+export const useGetAllProcedures = () => {
+  return useQuery({
+    queryKey: ['procedimientos'],
+    queryFn: () => getAllProcedures(),
+  })
+}
+
+export const useGetProceduresByEncounterId = (encounterId) => {
+  return useQuery({
+    queryKey: ['procedimientos', encounterId],
+    queryFn: () => getProcedureByEncounterId(encounterId),
+  })
+}
+
+export const useGetProceduresByPatientId = (patientId) => {
+  return useQuery({
+    queryKey: ['procedimientos', patientId],
+    queryFn: () => getProcedureByPatientId(patientId),
+  })
+}
+
+export const useGetProcedureByReference = (reference) => {
+  return useQuery({
+    queryKey: ['procedimiento', reference],
+    queryFn: () => getProcedureByReference(reference),
+  })
+}
+
+// Mutations
+export const useAddProcedure = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: addProcedure,
+    onSuccess: () => {
+      // Invalidamos la caché.
+      queryClient.invalidateQueries({ queryKey: ['procedimientos'] })
+    }
+  })
+}
