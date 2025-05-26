1d5a045dd9ef19a62ee9e57a2732c865553495e2
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-05-26 15:08:41 -0300

    Refactor LoginPractitioner para usar useForm y Zustand, simplificando el manejo de estado y la lógica de inicio de sesión

--- src/pages/LoginPractitioner.jsx
+++ src/pages/LoginPractitioner.jsx
@@ -1,77 +1,47 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import { getPractitionerByCIAction } from "../api/practitioner/get-practitioner";
 import OdontoFhirLogo from '../assets/logo.svg'
+import { usePractitionerStore } from "../storage/practitionerStore";
+import { useForm } from "react-hook-form";
 
 export const LoginPractitioner = () => {
-  const [practitionerId, setPractitionerId] = useState("");
-  const [submittedId, setSubmittedId] = useState(null);
+  const { register, handleSubmit } = useForm()
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate()
 
-  const [practitioner, setPractitioner] = useState({
-    isLoading: false,
-    isError: false,
-    isSuccess: false,
-    data: null,
-  });
+  // Usamos el store de Zustand para manejar el estado del profesional
+  const { practitioner, setPractitioner } = usePractitionerStore()
 
-  useEffect(() => {
-    if (!submittedId) return;
+  const onSubmit = async (data) => {
+    await obtenerDatosPractitioner(data.practitionerId);
+  }
 
-    setPractitioner({
-      isLoading: true,
-      isError: false,
-      isSuccess: false,
-      data: null,
-    });
-
-    getPractitionerByCIAction(submittedId)
-      .then((data) => {
-        setPractitioner({
-          isLoading: false,
-          isError: false,
-          isSuccess: true,
-          data,
-        });
-      })
-      .catch(() => {
-        setPractitioner({
-          isLoading: false,
-          isError: true,
-          isSuccess: false,
-          data: null,
-        });
+  const obtenerDatosPractitioner = async (practitionerIdentification) => {
+    setLoading(true);
+    const practitionerData = await getPractitionerByCIAction(practitionerIdentification);
+    if (practitionerData && practitionerData.entry && practitionerData.entry.length > 0) {
+      setPractitioner({
+        isLoading: false,
+        isError: false,
+        isSuccess: true,
+        data: practitionerData.entry[0],
       });
-  }, [submittedId]);
-
-  useEffect(() => {
-    if (practitioner.isError) {
-      toast.error("Error al iniciar sesión. Por favor, intente nuevamente.");
-    }
-
-    if (practitioner.isSuccess && practitioner.data && practitioner.data.total > 0) {
-      console.log("Datos del profesional:", practitioner.data);
       toast.success("¡Bienvenido, profesional! Que sea un buen día. 🌤️");
+      setLoading(false);
       navigate('/');
-    } else if (practitioner.data && practitioner.data.total === 0) {
-      console.log("No se encontró el profesional o no hay datos disponibles.");
+    } else {
+      setPractitioner({
+        isLoading: false,
+        isError: true,
+        isSuccess: false,
+        data: null,
+      });
+      setLoading(false);
       toast.error("No se encontró el profesional. Por favor, verifique su Registro Profesional o CI.");
     }
-  }, [practitioner.isError, practitioner.isSuccess, practitioner.data, navigate]);
-
-  const handleInputChange = (e) => {
-    setPractitionerId(e.target.value);
-  };
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    if (!practitionerId) {
-      toast.error("Por favor, ingrese su Registro Profesional o CI.");
-      return;
-    }
-    setSubmittedId(practitionerId);
-  };
+  }
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
@@ -80,7 +50,7 @@ export const LoginPractitioner = () => {
       </header>
       <main className="flex flex-col gap-4 items-center justify-center bg-white p-6 rounded-md shadow-lg">
         <h1 className="font-semibold text-2xl text-[#4a4a4a]">Bienvenido, profesional</h1>
-        <form className="flex flex-col gap-4 min-w-md max-w-2xl" onSubmit={handleLogin}>
+        <form className="flex flex-col gap-4 min-w-md max-w-2xl" onSubmit={handleSubmit(onSubmit)}>
           <label className="font-medium text-[#4a4a4a]">
             Registro Profesional o CI
             <input
@@ -88,16 +58,15 @@ export const LoginPractitioner = () => {
               name="practitionerId"
               placeholder="Ingrese su Registro Profesional o CI"
               className="border border-gray-300 rounded p-2 w-full"
-              value={practitionerId}
-              onChange={handleInputChange}
+              {...register("practitionerId", { required: true })}
             />
           </label>
           <button
             type="submit"
             className="flex justify-center gap-2 end-0 px-4 py-2 rounded-md border-2 border-gray-600 bg-[#B3E5FC] text-[#4A4A4A] font-semibold hover:cursor-pointer hover:bg-[#86cff0] hover:scale-95 duration-100 transition-all"
-            disabled={practitioner.isLoading}
+            disabled={loading}
           >
-            {practitioner.isLoading ? "Cargando..." : "Iniciar sesión"}
+            {loading ? "Cargando..." : "Iniciar sesión"}
           </button>
         </form>
         <a href="/register" className="text-[#4A4A4A] hover:text-[#86cff0] transition-colors duration-200">
