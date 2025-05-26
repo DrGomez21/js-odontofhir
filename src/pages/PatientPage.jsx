4c468e44135d8e8be1ae68272aca71b387da8e42
Diego <99410932+DrGomez21@users.noreply.github.com>
2025-05-26 15:08:49 -0300

    Agregar importación de usePractitionerStore y comentarios de ejemplo en PatientPage.jsx

--- src/pages/PatientPage.jsx
+++ src/pages/PatientPage.jsx
@@ -3,9 +3,13 @@ import { PatientTable } from '../components/PatientTable'
 import { Link } from 'react-router-dom'
 import { Header } from '../components/basics/Header'
 import { BotonConPlus, BotonConFolder } from '../components/boton/boton';
-
+import { usePractitionerStore } from '../storage/practitionerStore';
 
 export const PatientPage = () => {
+
+  // const { data: practitioner } = usePractitionerStore((state) => state.practitioner);
+  // console.log('Practitioner data:', practitioner);
+
   return (
     <div className='flex flex-col overflow-y-hidden'>
 
@@ -18,6 +22,8 @@ export const PatientPage = () => {
           </BotonConPlus>
         </Link>
       </div>
+      {/* Ejemplo de como acceder a los datos del practitioner: */}
+      {/* <p>Hola, {practitioner.resource.name[0].given[0]} {practitioner.resource.name[0].family}</p> */}
 
       <div className='mx-12 mt-4'>
         <PatientTable />
